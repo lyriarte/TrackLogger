a21fe83c534849412075651022e846396c6a9f7f
Luc Yriarte <lyriarte@gmail.com>
2015-03-15 15:42:39 +0100

    Get GPS location changes

--- app/src/main/java/org/yriarte/tracklogger/TrackLogger.java
+++ app/src/main/java/org/yriarte/tracklogger/TrackLogger.java
@@ -1,21 +1,34 @@
 package org.yriarte.tracklogger;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import java.sql.Timestamp;
-import java.util.Calendar;
 
 
-public class TrackLogger extends ActionBarActivity {
+public class TrackLogger extends ActionBarActivity implements LocationListener {
+
+    static int minTimeUpdateSeconds = 30;
+    static float minDistanceUpdateMeters = 5;
+
+    LocationManager mLocationManager;
+    Location mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_logger);
-        updateTrackPoint(43.611769,4.010320,3,Calendar.getInstance().getTimeInMillis());
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        mLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (mLocation == null)
+            return;
+        updateTrackPoint(mLocation.getLatitude(),mLocation.getLongitude(),mLocation.getAltitude(),mLocation.getTime());
     }
 
 
@@ -35,9 +48,14 @@ public class TrackLogger extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_log_start) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    minTimeUpdateSeconds * 1000,
+                    minDistanceUpdateMeters,
+                    this);
             return true;
         }
         if (id == R.id.action_log_stop) {
+            mLocationManager.removeUpdates(this);
             return true;
         }
 
@@ -51,4 +69,24 @@ public class TrackLogger extends ActionBarActivity {
         ((TextView)findViewById(R.id.value_time)).setText(new Timestamp(time).toString());
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        updateTrackPoint(mLocation.getLatitude(),mLocation.getLongitude(),mLocation.getAltitude(),mLocation.getTime());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
